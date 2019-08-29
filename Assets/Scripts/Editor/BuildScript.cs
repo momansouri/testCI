1f3db185302f39f365c3832f035a5cc0899f4325
mmansouri <mohamed.mansouri@sky.uk>
2019-08-29 13:42:22 +0100

    first commit

---
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildScript
+{
+    public static void Android()
+    {
+        var buildPlayerOptions = new BuildPlayerOptions
+        {
+            locationPathName = @"c:\temp\mmansouri.apk",
+            options = BuildOptions.AllowDebugging,
+            target = BuildTarget.Android,
+            targetGroup = BuildTargetGroup.Android,
+            scenes = GetScenes()            
+        };
+
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+
+    public static void Package()
+    {
+        AssetDatabase.ExportPackage(@"Assets\Scenes", "mmansouri.unitypackage", ExportPackageOptions.Default);
+    }
+    private static string[] GetScenes()
+    {
+        return EditorBuildSettings.scenes
+            .Where(s => s.enabled)
+            .Select(s => s.path)
+            .ToArray();
+    }
+}
---
+++ BuildScripts/build.sh
@@ -0,0 +1,3 @@
+echo 'starting build scripts'
+unity.exe -batchmode -projectPath c:\git\skyvr\TestCI -executeMethod BuildScript.BuildGame
+echo 'ending build scripts'
