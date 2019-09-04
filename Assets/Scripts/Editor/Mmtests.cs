74949e3af22586feeda93777a2ba2e9e0e0667f8
mmansouri <mohamed.mansouri@sky.uk>
2019-09-04 22:10:50 +0100

    added unit tests

---
+++ Assets/Scripts/Editor/Mmtests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+public class Mmtests
+{
+    [Test]
+    public void DummyTests()
+    {
+        Assert.AreEqual(1, 2-1);
+    }
+
+    [Test]
+    public void FailingTests()
+    {
+        Assert.AreEqual(1, 2);
+    }
+}
